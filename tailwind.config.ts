5d92632e84e213f351e7f3db11f11c232b7ba3a5
Miloshinjo <miloshdz@gmail.com>
2023-08-31 17:10:22 +0200

    Save config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,50 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
+import { z } from 'zod';
+
+const themeSchema = z.union([
+  z.literal('default'),
+  z.literal('pink'),
+  z.literal('blue'),
+]);
+
+const defaultTheme = {
+  primary: '#383838',
+  secondary: '#00ffff',
+  accent: '#383838',
+  background: '#ffffff',
+  text: '#000000',
+};
+
+const pinkTheme = {
+  primary: '#ff00ff',
+  secondary: '#00ffff',
+  accent: '#ff00ff',
+  background: '#000000',
+  text: '#ffffff',
+};
+
+const blueTheme = {
+  primary: '#0000ff',
+  secondary: '#00ffff',
+  accent: '#0000ff',
+  background: '#ffffff',
+  text: '#000000',
+};
+
+const themes: Record<z.infer<typeof themeSchema>, any> = {
+  default: defaultTheme,
+  pink: pinkTheme,
+  blue: blueTheme,
+};
+
+const parsedTheme = themeSchema.safeParse(process.env.THEME);
+
+if (parsedTheme.success === false) {
+  console.error(parsedTheme.error);
+  process.exit(1);
+}
+
+const colors = themes[parsedTheme.data];
 
 const config: Config = {
   content: [
@@ -8,13 +54,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        ...colors,
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
